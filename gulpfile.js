870955b5e015126365193d4e6450b012e9124636
winston <mttylza@sina.com>
2017-03-31 15:16:58 +0800

    提供了对compass 支持

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,31 @@
 var gulp = require('gulp');
 var del = require('del');
 var rename = require('gulp-rename');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var compass = require('gulp-compass');
 
 gulp.task('clean',function(){
     del(['./dist'])
 });
+gulp.task('compass', function() {
+    gulp.src(['./src/**/*.scss'])
+        .pipe(compass({
+            config_file: './config.rb',
+            logging:true,
+            sass:'src',
+            css: './build/temp/css',
+            generated_images_path:'./build/temp/images'
+        }))
+        .pipe(rename({extname: ".wxss"}))
+        .pipe(gulp.dest('dist'))
+});
 gulp.task('wxss', function() {
-    gulp.src(['./src/**/*.css','./src/**/*.wxss'])
+    gulp.src(['./src/**/*.css','.n/src/**/*.wxss'])
         .pipe(rename({extname: ".wxss"}))
         .pipe(gulp.dest('dist'));
 });
 gulp.task('wxml', function() {
-    gulp.src(['./src/**/*.xml','./src/**/*.wxml'])
+    gulp.src(['./src/**/*.xml','./src/**/*.wxml','./src/**/*.html'])
         .pipe(rename({extname: ".wxml"}))
         .pipe(gulp.dest('dist'));
 });
@@ -35,11 +48,11 @@ gulp.task('img', function() {
 
 gulp.task('watch',function() {
     gulp.watch([
-        './src/**/*.css','./src/**/*.wcss',
-        './src/**/*.xml','./src/**/*.wxml',
+        './src/**/*.css','./src/**/*.wcss','./src/**/*.scss',
+        './src/**/*.xml','./src/**/*.wxml','./src/**/*.html',
         './src/**/*.js',
         './src/**/*.json',
         './src/**/*.png','./src/**/*.img'
-    ], ['wxss','wxml','js','json','img']);
+    ], ['compass','wxss','wxml','js','json','img']);
 })
-gulp.task('default',['watch','wxss','wxml','js','json','img']);
+gulp.task('default',['watch','compass','wxss','wxml','js','json','img']);
